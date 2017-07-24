a41e3d64e770d8c69ef76df682b25dd0e058e67a
Denasia Hamilton <Denasia Hamilton>
2017-07-24 11:56:19 -0700

    added

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import logging
+import os
+import jinja2
+import json
+import urllib
+import urllib2
 import webapp2
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
-class MainHandler(webapp2.RequestHandler):
+jinja_environment = jinja2.Environment(
+    loader= jinja2.FileSystemLoader(
+        os.path.dirname(__file__) + '/templates')
+        )
+
+"""class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+"""
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        cur_user = users.get_current_user()
+        logging.info(str(cur_user))
+        log_url = ''
+        if cur_user:
+            log_url = users.create_logout_url('/login')
+        else:
+            log_url = users.create_login_url('/')
+            logging.info(log_url)
+        template = jinja_environment.get_template('main.html')
+        var = {
+            'user' :cur_user,
+            'log_url': log_url
+        }
+        self.response.out.write(template.render(var))
+
+class HomePageHandler(webapp2.RequestHandler):
+    def get(self):
+
+            template = jinja_environment.get_template('homepage.html')
+            self.response.out.write(template.render())
+
+class ChooseOutfitHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+class StylesColorsHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    #('/', MainHandler),
+    ('/login', LoginHandler),
+    ('/', HomePageHandler),
+    ('/choose_outfit', ChooseOutfitHandler),
+    ('/styles_colors', StylesColorsHandler)
+
 ], debug=True)
